3b510a87e0e62356e690096f977d6a013ed96113
Hariz Yusoff <hariz@snappymob.com>
2021-05-10 12:55:38 +0800

    Setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { UsersController } from './users/controller/users.controller';
 import { AdminController } from './admin/admin.controller';
 import { AccountController } from './account/account.controller';
 import { UsersService } from './users/service/users.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConnectionService } from './db-connection.service';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({
+    useClass: DatabaseConnectionService
+  })],
   controllers: [AppController, UsersController, AdminController, AccountController],
   providers: [AppService, UsersService],
 })
